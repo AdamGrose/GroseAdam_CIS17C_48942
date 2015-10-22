340b03d92b0526841c94c4aba6a408ef05a01143
AdamGrose <adam.grose1@gmail.com>
2015-10-22 12:50:20 -0700

    Signed-off-by: AdamGrose <adam.grose1@gmail.com>

---
+++ hmwk/LinkList/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on October 22, 2015, 11:08 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+};
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
